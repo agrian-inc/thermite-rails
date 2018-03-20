562cf288cad474681d9bf4af9e92a80b21a456e4
Steve Loveless <steve@agrian.com>
2018-03-20 11:07:57 -0700

    Initial commit

---
+++ lib/thermite/rails.rb
@@ -0,0 +1,7 @@
+require "thermite/rails/version"
+
+module Thermite
+  module Rails
+    # Your code goes here...
+  end
+end
---
+++ lib/thermite/rails/version.rb
@@ -0,0 +1,5 @@
+module Thermite
+  module Rails
+    VERSION = "0.1.0"
+  end
+end
